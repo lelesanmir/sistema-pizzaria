a4e25e34605af1c5d611e3734befe8005960240b
lelesanmir <leonardodossantosmiranda@gmail.com>
2024-12-26 23:34:32 -0300

    Criado o service para criar um usuario e oculta a senha dele na requisicao.

--- backend/src/services/user/CreateUserService.ts
+++ backend/src/services/user/CreateUserService.ts
@@ -1,15 +1,49 @@
-interface UserRequest{
+import { PrismaClient } from '@prisma/client';
+
+interface UserRequest {
     name: string;
     email: string;
     password: string;
 }
 
-class CreateUserService{
-    async execute({ name, email, password } : UserRequest){
-       
+const prisma = new PrismaClient();  // Instanciando o Prisma Client
+
+class CreateUserService {
+    async execute({ name, email, password }: UserRequest) {
+        // Verificar se o email foi fornecido
+        if (!email) {
+            throw new Error("Email incorreto");
+        }
+
+        // Verificar se o email já está cadastrado
+        const userAlreadyExists = await prisma.user.findUnique({
+            where: {
+                email: email,  // Procurando usuário pelo campo 'email', que é único
+            },
+        });
+
+        if (userAlreadyExists) {
+            throw new Error("User Already Exists");
+        }
+
+        // Criar o novo usuário
+        const user = await prisma.user.create({
+            data: {
+                name: name,
+                email: email,
+                password: password,
+            },
+            select:{
+                id: true,
+                name: true,
+                email: true,
+            }
+        });
+
         console.log(name);
-       
-        return { name: name };
+
+        return user;
     }
 }
-export { CreateUserService }
+
+export { CreateUserService };
