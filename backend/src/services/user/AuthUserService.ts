b4515e08b8eea96215ddddae600a1708bc910780
lelesanmir <leonardodossantosmiranda@gmail.com>
2024-12-27 23:08:47 -0300

    Criado login user e verificaçoes no banco de dados para saber se existe o mesmo!

---
+++ backend/src/services/user/AuthUserService.ts
@@ -0,0 +1,37 @@
+import prismaClient from "../../prisma";
+import { compare } from 'bcryptjs'
+
+
+
+interface AuthRequest{
+    email: string;
+    password: string;
+}
+
+class AuthUserService{
+    async execute({ email, password }: AuthRequest){
+        // verificar se o email existe
+        const user = await prismaClient.user.findFirst({
+            where:{
+                email: email
+            }
+        })
+        
+        if(!user){
+            throw new Error("User/password incorrect")
+        }
+
+        //preciso verificar ser a senha que ele mandou está correta.
+        const passwordMatch = await compare(password, user.password);
+
+        if(!passwordMatch){
+            throw new Error("User/Password incorrect")
+        }
+
+        
+
+        return { ok: true }
+    }
+}
+
+export { AuthUserService }
