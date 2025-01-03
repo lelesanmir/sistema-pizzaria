074332c706e764214bbb31ff765db8cf66e0cefd
lelesanmir <leonardodossantosmiranda@gmail.com>
2025-01-02 21:13:02 -0300

    adicioando o tokenjwtweb no projeto.

--- backend/src/services/user/AuthUserService.ts
+++ backend/src/services/user/AuthUserService.ts
@@ -1,5 +1,6 @@
 import prismaClient from "../../prisma";
 import { compare } from 'bcryptjs'
+import { sign } from 'jsonwebtoken'
 
 
 
@@ -28,9 +29,25 @@ class AuthUserService{
             throw new Error("User/Password incorrect")
         }
 
-        
+        // Se deu tudo certo vamos gerar o token para o usuário
+        const token = sign(
+            {
+                name : user.name,
+                email : user.email
+            },
+            process.env.JWT_SECRET,
+            {
+                subject:user.id,
+                expiresIn: '30d'
+            }
+        )
 
-        return { ok: true }
+        return { 
+            id: user.id,
+            name: user.name,
+            email: user.email,
+            token: token
+        }
     }
 }
 
